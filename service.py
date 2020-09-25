1015dc16519c36119271e061047b008050fa3af7
Garrett Hagen <gbh8@pitt.edu>
2020-09-24 20:45:56 -0700

    --amend

--- service.py
+++ service.py
@@ -345,23 +345,4 @@ class Descriptor(dbus.service.Object):
         self.value = value
 
 
-class CharacteristicUserDescriptionDescriptor(Descriptor):
-    CUD_UUID = '2901'
 
-    def __init__(self, bus, index, characteristic):
-        self.writable = 'writable-auxiliaries' in characteristic.flags
-        self.value = array.array('B', b'This is a characteristic for testing')
-        self.value = self.value.tolist()
-        Descriptor.__init__(
-            self, bus, index,
-            self.CUD_UUID,
-            ['read', 'write'],
-            characteristic)
-
-    def ReadValue(self, options):
-        return self.value
-
-    def WriteValue(self, value, options):
-        if not self.writable:
-            raise NotPermittedException()
-        self.value = value

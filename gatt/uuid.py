9a98bae3df4d31312080306ced5f0d44831f271a
Garrett Hagen <gbh8@pitt.edu>
2020-09-30 20:42:38 -0700

    uuid and minor chages

--- gatt/uuid.py
+++ gatt/uuid.py
@@ -13,11 +13,17 @@
 class UUID(object):
     EXTENDED_FILLER = '0000-1000-8000'
 
-    def __init__(self, attribute_uid:str, device_uid:str='0001', company_uid:str='1421', ble_sig_uid:str='00805F9B'):
+    def __init__(self, attribute_uid: str, device_uid: str = '0808', company_uid: str = '1421',
+                 ble_sig_uid: str = '00805F9B'):
         self.attribute_uid = attribute_uid
         self.device_uid = device_uid
         self.company_uid = company_uid
         self.ble_sig_uid = ble_sig_uid
 
+        if len(attribute_uid) != 4: raise Exception("attribute_id must be a hex value of length 4")
+        if len(device_uid) != 4: raise Exception("device_uid must be a hex value of length 4")
+        if len(company_uid) != 4: raise Exception("company_uid must be a hex value of length 4")
+        if len(ble_sig_uid) != 8: raise Exception("ble_sig_id must be a hex value of length 8")
+
     def full_string(self):
-        return self.device_uid+self.attribute_uid+'-'+self.EXTENDED_FILLER+"-"+self.ble_sig_uid+self.company_uid
+        return self.device_uid + self.attribute_uid + '-' + self.EXTENDED_FILLER + "-" + self.ble_sig_uid + self.company_uid
